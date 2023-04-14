1aa728b9c87a4b72f5d1ef4c1bb1547e9e2a7b1b
JOSE_SAD <zegui15@gmail.com>
2023-04-13 22:21:07 -0300

    pagina tarefa nova e alteracoes em home

--- src/app/tarefa-nova/tarefa-nova.page.ts
+++ src/app/tarefa-nova/tarefa-nova.page.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonicModule } from '@ionic/angular';
+import { IonicModule, ModalController } from '@ionic/angular';
+import { TarefaService } from '../services/tarefa.service';
+import { Tarefa } from '../models/tarefa.model';
 
 @Component({
   selector: 'app-tarefa-nova',
@@ -11,10 +13,48 @@ import { IonicModule } from '@ionic/angular';
   imports: [IonicModule, CommonModule, FormsModule]
 })
 export class TarefaNovaPage implements OnInit {
+  public categorias = [ 'domestico', 'pessoal', 'trabalho' ];
+  public categoriaSelecionada: string = '';
 
-  constructor() { }
+  public dataSelecionada: string = new Date().toISOString();
+
+  public minhaTarefa: Tarefa = {
+    id: '',
+    nome: '',
+    data: '',
+    prioridade: 'alto',
+    categoria: ''
+  };
+
+  constructor(private modalCtrl: ModalController, public tarefaServ: TarefaService) { }
 
   ngOnInit() {
+  }
+
+  public async adicionar() {
+    let uid: any = Date.now();
+    uid = uid.toString(16);
+
+    this.minhaTarefa.id = uid;
+
+    this.minhaTarefa.data = this.dataSelecionada;
+    this.minhaTarefa.categoria = this.categoriaSelecionada;
+
+    
+    if (this.minhaTarefa.nome && this.minhaTarefa.data && this.categoriaSelecionada) {
+      await this.tarefaServ.adicionar(this.minhaTarefa);
+    }
+
+    this.dismis();
+  }
+
+  
+  public async dismis() {
+    await this.modalCtrl.dismiss(this.minhaTarefa);
+  }
+  
+  public selecionarCategoria(index: number) {
+    this.categoriaSelecionada = this.categorias[index];
   }
 
 }
